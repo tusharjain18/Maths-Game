83000bee803c8d9613b51037b7e1682469e945b4
Tushar Jain <78149390+tusharjain18@users.noreply.github.com>
2022-06-16 23:11:39 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,10 +2,11 @@ var playing = false;
 var score;
 var action;
 var timeremaining;
+var correctAnswer;
 
 // click on start/reset
 document.getElementById("startreset").onclick = 
-    function(){
+function(){
     // if we are playing
     if(playing == true){
         location.reload();//reload page
@@ -38,6 +39,30 @@ document.getElementById("startreset").onclick =
     }
 }
 
+//clicking on answer box
+for(i=1;i<5;i++){
+    document.getElementById("box"+i).onclick=function(){
+        if(playing == true){
+            if(this.innerHTML == correctAnswer){
+                score++;
+                document.getElementById("scorevalue").innerHTML=score;
+                hide("wrong");
+                show("correct");
+                setTimeout(function(){
+                    hide("correct");
+                },1000);
+        
+                generateQA();
+            }else{
+                hide("correct");
+                show("wrong");
+                setTimeout(function(){
+                    hide("wrong");
+                },1000);
+            }
+        }
+    }
+}
 function startCountdown(){
     action = setInterval(function(){
         timeremaining -= 1;
@@ -47,7 +72,7 @@ function startCountdown(){
             
             show("gameOver");
             
-            document.getElementById("gameOver").innerHTML = "<p>Game Over!</p><p>Your Score is " + score +".</p>";
+            document.getElementById("gameOver").innerHTML = "<p>Game Over!</p><p>Your Score is " + score + ".</p>";
             
             hide("timeremaining");
             hide("correct");
@@ -75,5 +100,24 @@ function show(Id){
 
 //generate question
 function generateQA(){
+    var x = 1+ Math.round(9*Math.random());
+    var y = 1+ Math.round(9*Math.random());
+    correctAnswer = x*y;
+    document.getElementById("question").innerHTML = x + "x" + y;
+    var correctPosition = 1+ Math.round(3*Math.random());
+    document.getElementById("box"+correctPosition).innerHTML = correctAnswer; 
+    // fill one box with correct answer 
     
+    //fill other box with wrong answer
+    var answers = [correctAnswer];
+    for(i=1; i<5; i++){
+        if(i != correctPosition){
+            var wrongAnswer;
+            do{
+                  wrongAnswer = (1+Math.round(9*Math.random()))* (1+Math.round(9*Math.random()));
+            }while(answers.indexOf(wrongAnswer)>-1)
+            document.getElementById("box"+i).innerHTML = wrongAnswer;
+            answers.push(wrongAnswer);
+        }
+    }
 }
